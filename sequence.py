135be1d2ed2a75fcd50e126abd0d08d505047ba0
Sergei Dolgov <dolgovs@gmail.com>
2013-10-21 19:43:00 +0200

    delete-trailing-whitespace

--- sequence.py
+++ sequence.py
@@ -5,28 +5,28 @@ class Sequence(object):
         self._title = title
         self._number = number
         self._entries = []
-    
+
     def has_posts(self):
         return len(self._entries)
-    
+
     def filename(self):
         return self._entries[0].filename
-    
+
     def title(self):
         return self._title
 
     def full_title(self):
         return "Sequence {:02d}: {}".format(self._number, self._title)
-    
+
     def append(self, p):
         self._entries.append(p)
-    
+
     def before(self, p):
         ix = self._entries.index(p)
         if ix == 0:
             return None
         return self._entries[ix -1]
-    
+
     def after(self, p):
         ix = self._entries.index(p)
         if ix+1 == len(self._entries):
